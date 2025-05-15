d1da9a35adb3043c90c9771a8007f963b776a54a
Yuna <yuna00708@gmail.com>
2025-05-15 15:03:38 +0900

    :lipstick: style: apply theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import theme from '@/shared/theme/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import { AppRoutes } from './app/AppRoutes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRoutes />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppRoutes />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
