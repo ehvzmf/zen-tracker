5bcb6868870daf4669c8f9b2bfd3280d67273da9
Yuna <yuna00708@gmail.com>
2025-05-15 15:12:42 +0900

    :fire: chore: remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import theme from '@/shared/theme/theme';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { AppRoutes } from './app/AppRoutes'
 
