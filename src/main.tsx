bef44bc601c1948e19162891039db9793e8d2d03
Yuna Choi <mia@Yunaui-Macmini.local>
2025-06-25 23:39:03 +0900

    :heavy_plus_sign: feat: add queryClient and provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import theme from '@/shared/theme/theme';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { AppRoutes } from './app/AppRoutes'
 import './index.css'
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <AppRoutes />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
 );
