8e31d8cbf8186d5fedfa276f7276d67dfdb4c117
ehvzmf <yuna00708@gmail.com>
2025-07-13 23:58:16 +0900

    :sparkles: feat: custom button

---
+++ src/shared/ui/Button.tsx
@@ -0,0 +1,15 @@
+import { Box, Button, IconButton, Typography } from '@mui/material';
+import { useState } from 'react';
+
+interface ButtonProps {
+  title: string;
+  iconSrc: string;
+  type: string;
+}
+
+export const CustomButton = ({ title, iconSrc, type }: ButtonProps) => {
+  return (
+    // 너비, 기능, 색상에 따른 다양한 타입의 버튼 확장
+    // type에 따라 다른 스타일 적용
+  )
+}
