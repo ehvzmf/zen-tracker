a966cf68fd7ea673a0ab8f785cb7ca83eb338269
ehvzmf <yuna00708@gmail.com>
2025-08-29 21:11:47 +0900

    fix: import home

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,16 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from '@/widgets/Layout';
-import { Home } from '@/pages/Home';
+import Home from '@/pages/home';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
     children: [
-      {
-        index: true,
-        element: <Home />,
-      },
+      { index: true, element: <Home /> },
     ],
   },
 ]);
